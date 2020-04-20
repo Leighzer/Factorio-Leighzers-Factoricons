9b666d2e792610390b4862df330e735c303a8fe8
Leighzer <bwc_nothgiel@live.com>
2020-04-19 21:40:33 -0400

    v0.1.1

--- Source/categories.lua
+++ Source/categories.lua
@@ -1,4 +1,4 @@
-leighzermods.utils.createItemGroup("leighzerfactoricons","zz","zz","__leighzerlib__/graphics/item-group/factoricon-item-group.png","Factoricons")
+leighzermods.utils.createItemGroup("leighzerfactoricons","zz","zz","__leighzerlib__/graphics/item-group/factoricon-item-group.png",128,"Factoricons")
 
 leighzermods.utils.createItemSubgroup("leighzerfactoricons-top-row","leighzerfactoricons","a")
 leighzermods.utils.createItemSubgroup("leighzerfactoricons","leighzerfactoricons","b")
