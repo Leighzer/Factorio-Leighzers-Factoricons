52b33104688312dd7cb7c0602aca0a7482660ac9
Leighzer <bwc_nothgiel@live.com>
2020-12-17 23:54:58 -0500

    v0.3.0;

--- Source/migrations/leighzerfactoricons_0.1.0.lua
+++ Source/migrations/leighzerfactoricons_0.1.0.lua
@@ -2,5 +2,4 @@ for _,force in pairs(game.forces) do
     if force.recipes["space-science-pack"] then
         force.recipes["space-science-pack"].enabled = force.technologies["space-science-pack"].researched
     end
-    
 end
